d988ded0f41bb6fb39e50ade55835759225e772c
Ted Moore <ted@tedmooremusic.com>
2024-01-07 15:32:40 -0500

    refactor and include masking resynthesis strategy

--- python/nmf_decomposition.py
+++ python/nmf_decomposition.py
@@ -9,9 +9,26 @@ from pathlib import Path
 import json
 import time
 
+def newMagsViaSoftMask(mags,matrix):
+    multiplier = 1 / np.maximum(np.sum(mags,axis=0),1e-10) # epsilon to avoid divide by zero
+    print('multiplier:',multiplier)
+    print('multiplier shape:',multiplier.shape)
+    masks = [mags[i] * multiplier for i in range(len(mags))]
+    masked_mags = [np.transpose(matrix) * masks[i] for i in range(len(mags))]
+    return masked_mags
+
+def getMags(activation,basis):
+    return np.outer(basis, activation)
+
+def combineMagsAndPhases(mags,phases):
+    return mags * np.exp(1j * phases)
+
+def istft(stft,fftSettings):
+    return librosa.istft(stft,**fftSettings)
+    
 def resynthComponent(stft,activation,basis,outpath,fftSettings,sr):
-    Y = np.outer(basis, activation) * np.exp(1j * np.angle(stft))
-    y = librosa.istft(Y,**fftSettings)
+    Y = combineMagsAndPhases(getMags(activation,basis),np.angle(stft))
+    y = istft(Y,fftSettings)
     sf.write(outpath,y,sr,subtype='PCM_24')
 
 def plot_nmf(stft, acts, bases, sr):
@@ -34,7 +51,7 @@ def plot_nmf(stft, acts, bases, sr):
     plt.tight_layout()
     plt.show()
 
-def main(audio_path, n_components,fftSize,hopSize,resynth,plot,duration_seconds):
+def decompose(audio_path, n_components,fftSize,hopSize,resynth,plot,duration_seconds,soft_mask):
 
     fftSettings = {'n_fft':fftSize,'hop_length':hopSize}
 
@@ -78,8 +95,16 @@ def main(audio_path, n_components,fftSize,hopSize,resynth,plot,duration_seconds)
     print(f'bases shape: {bases.shape}')
 
     if resynth:
-        for i in range(n_components):
-            resynthComponent(stft,acts[i],bases[i],f'component-{i}.wav',fftSettings,sr)
+        if soft_mask:
+            mags = [getMags(acts[i],bases[i]) for i in range(n_components)]
+            masked_mags = newMagsViaSoftMask(mags,matrix)
+            for i in range(n_components):
+                print('masked mags shape:',masked_mags[i].shape)
+                y = istft(combineMagsAndPhases(masked_mags[i],np.angle(stft)),fftSettings)
+                sf.write(f'component-{i}.wav',y,sr,subtype='PCM_24')
+        else:
+            for i in range(n_components):
+                resynthComponent(stft,acts[i],bases[i],f'component-{i}.wav',fftSettings,sr)
 
     if plot:
         plot_nmf(stft, acts, bases, sr)
@@ -93,8 +118,9 @@ if __name__ == '__main__':
     parser.add_argument('--resynth',action='store_true',help='boolean flag to resynthesize individual components after decomposition')
     parser.add_argument('--plot',action='store_true',help='boolean flag to plot spectrogram, bases, and activations')
     parser.add_argument('--duration-seconds',type=float,required=False,help='duration of audio to analyze in seconds')
+    parser.add_argument('--soft-mask',action='store_true',help='boolean flag to use soft mask')
     args = parser.parse_args()
 
     args.hopSize = int(args.fftSize / 2) if args.hopSize is None else args.hopSize
 
-    main(**vars(args))
+    decompose(**vars(args))
