7adff4fe6807baa10fa9d018ad5bc4fd07e2bad2
Ted Moore <ted@tedmooremusic.com>
2024-02-28 19:38:03 -0500

    simplified nmf_resynth.py

--- python/nmf_resynth.py
+++ python/nmf_resynth.py
@@ -3,33 +3,6 @@ import numpy as np
 import argparse
 import json
 import librosa
-from nmf_decomposition import make_mags_from_basis_and_activation
-from nmf_decomposition import make_complex_matrix_from_mags_and_phases
-from nmf_decomposition import istft
-from nmf_decomposition import write_to_file
-
-def resynth_from_arbitrary_a_and_b(phases,activation,basis,fftSettings,sr):
-    new_mags = make_mags_from_basis_and_activation(activation,basis)
-    complex_matrix = make_complex_matrix_from_mags_and_phases(new_mags,phases)
-    return istft(complex_matrix,fftSettings), sr
-
-def resynthFromFiles(bases_data_path,activations_data_path,basis_index,activation_index):
-    with open(bases_data_path,'r') as f:
-        bases_data = json.loads(f.read())
-
-    with open(activations_data_path,'r') as g:
-        activations_data = json.loads(g.read())
-
-    fftSettings = {'n_fft':bases_data['n_fft'],'hop_length':bases_data['hop_length']}
-    
-    audio_buffer, _ = librosa.load(activations_data['audio_path'],sr=None,mono=True)
-    stft = librosa.stft(audio_buffer,**fftSettings)
-    phases = np.angle(stft)
-
-    activation = np.array(activations_data['acts'][activation_index])
-    basis = np.array(bases_data['bases'][basis_index])
-                   
-    return resynth_from_arbitrary_a_and_b(phases,activation,basis,fftSettings,bases_data['sr'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='NMF resynthesis')
@@ -40,5 +13,16 @@ if __name__ == '__main__':
     parser.add_argument('--output-path',type=str,required=True,help='path to write resynthesized audio file')
     args = parser.parse_args()
 
-    y, sr = resynthFromFiles(args.bases_data,args.activations_data,args.basis_index,args.activation_index)
-    write_to_file(y,args.output_path,sr)
+    with open(args.bases_data,'r') as f:
+        bases_data = json.loads(f.read())
+
+    with open(args.activations_data,'r') as f:
+        activations_data = json.loads(f.read())
+
+    basis = bases_data['bases'][args.basis_index]
+    activations = activations_data['acts'][args.activation_index]
+
+    resynthesized_mags = np.outer(basis,activations)
+    y = librosa.griffinlim(resynthesized_mags,hop_length=activations_data['hop_length'])
+    sf.write(args.output_path,y,bases_data['sr'],subtype='PCM_24')
+    
